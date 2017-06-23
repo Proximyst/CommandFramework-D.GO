868ae7a34859568f6107397ee3e1962cbd91c57a
Mariell <proximyst@proximyst.com>
2017-06-23 16:02:49 +0200

    Add initial CommandManager

---
+++ CommandManager.go
@@ -0,0 +1,10 @@
+package commandframework_discordgo
+
+// CommandManager holds all command instances.
+type CommandManager struct {
+    // Prefix is the text before the command.
+    // Example: Prefix as '!', and the command being 'hi'. It'd be '!hi' in Discord.
+    Prefix string
+
+
+}
