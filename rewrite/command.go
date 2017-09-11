f64e67d398ef2c1688eb90945b559f21cfe77bb4
Proximyst <proximyst@proximyst.com>
2017-09-11 21:30:32 +0200

    Allow for custom messages depending on outcome

--- rewrite/command.go
+++ rewrite/command.go
@@ -3,5 +3,7 @@ package commandframework_discordgo
 type Command interface {
   Aliases() []string
 
+  Message() map[Outcome]string // allows for custom outcomes too.
+
   Execute(ctx *CommandContext) (outcome Outcome, err error)
 }
