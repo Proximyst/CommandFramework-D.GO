0da864dfffc038ac1c16118768e6df1a532171d8
Proximyst <proximyst@proximyst.com>
2017-09-13 23:00:48 +0200

    work.

--- rewrite/arguments.go
+++ rewrite/arguments.go
@@ -1,1 +1,22 @@
 package commandframework_discordgo
+
+type Argument struct {
+}
+
+type Arguments struct {
+	Internal []Argument
+	Position int
+}
+
+func (arr *Arguments) incr() {
+	arr.Position += 1
+}
+
+func (arr *Arguments) ParseString() string {
+	arr.incr()
+	return arr.ParseStringPos(arr.Position)
+}
+
+func (arr *Arguments) ParseStringPos(position int) string {
+	return ""
+}
