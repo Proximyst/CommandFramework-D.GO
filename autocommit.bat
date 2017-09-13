e38737b29abcaa367055f75655b5a5280f12a640
Proximyst <proximyst@proximyst.com>
2017-09-13 22:18:45 +0200

    work.

---
+++ autocommit.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+:COMMIT
+commit.bat work.
+TIMEOUT /T 30 /NOBREAK
+GOTO :COMMIT
--- rewrite/commandManager.go
+++ rewrite/commandManager.go
@@ -1,8 +1,19 @@
 package commandframework_discordgo
 
-import "github.com/bwmarrin/discordgo"
+import (
+  "github.com/bwmarrin/discordgo"
+  "errors"
+)
 
 type CommandManager struct {
   Commands []InternalCommand
-  Bot *discordgo.Session
+  Bot      *discordgo.Session
+  Prefix   string
+}
+
+func (manager *CommandManager) Ready() error {
+  if manager.Bot == nil {
+    return errors.New("No bot was defined, which is needed!")
+  }
+  return nil
 }
