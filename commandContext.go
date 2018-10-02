b524b1360607e28fa41a47feb4b7d6aaa07689e7
Mariell Hoversholm <proximyst@proximyst.com>
2018-10-02 16:28:29 +0200

    Document context

--- commandContext.go
+++ commandContext.go
@@ -2,9 +2,21 @@ package commandframework_discordgo
 
 import "github.com/bwmarrin/discordgo"
 
+// CommandContext represents the context of the command execution.
+//
+// This is handed to the command's handler.
 type CommandContext struct {
+	// Implementation represents the implementation of the command.
+	//
+	// This can be used to get the inner instance of the command, should it be needed.
 	Implementation *Command
-	Event          *discordgo.MessageCreate
-	Session        *discordgo.Session
-	Label          string
+
+	// Event represents the event of the message itself.
+	Event *discordgo.MessageCreate
+
+	// Session represents the session instance of the bot.
+	Session *discordgo.Session
+
+	// Label represents the label used to execute the command, of the registered names.
+	Label string
 }
